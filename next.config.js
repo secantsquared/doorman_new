5cbcb1a3296a8fa6ef192f1caaba640ea36a80fc
ryanboris <ryan.boris@gmail.com>
2019-03-28 11:55:11 -0400

    :arrow_up: upgrades deps

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
+require('dotenv').config();
+
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   webpack: config => {
-    // Fixes npm packages that depend on `fs` module
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ];
+
     config.node = {
       fs: 'empty'
-    }
+    };
 
-    return config
+    return config;
   }
-}
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { Grommet, Box, Button } from 'grommet';
-import { Github } from 'grommet-icons';
-
+import { Grommet, Box } from 'grommet';
 import Head from '../components/Head';
 import AppBar from '../components/AppBar';
+import Button from '../components/Button';
 
 const theme = {
   global: {
@@ -20,12 +20,7 @@ const Home = () => (
     <Head title="Home" />
     <AppBar />
     <Box fill justify="center" align="center">
-      <Button
-        primary
-        icon={<Github />}
-        label="Login with GitHub"
-        color="neutral-1"
-      />
+      <Button />
     </Box>
   </Grommet>
 );
